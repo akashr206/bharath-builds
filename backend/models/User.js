654f49da0b2eddb852df43ff2b588eeae34a5701
Akash R <akashr6514@gmail.com>
2026-09-18 09:15:00 +0530

    Creating User schema model

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+    },
+    preferredLanguage: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
